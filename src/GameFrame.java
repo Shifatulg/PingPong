8fddff9dc31ebf8c95dfe9481f0749c4b3facd6e
Shifat <sample@bths.edi>
2024-05-31 08:55:20 -0400

    Buttons WIP

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -7,21 +7,37 @@ import java.awt.event.*;
 public class GameFrame implements ActionListener {
     private JPanel panel;
     private Timer time;
+    private JButton normal;
+    private JButton twin;
+    private JButton chaos;
+    private JButton gravity;
+    private JFrame frame;
     public GameFrame() {
-        JFrame frame = new JFrame("Pong");
+        frame = new JFrame("Pong");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1500, 1000);
-        frame.setLocationRelativeTo(null); // auto-centers frame in screen
+        frame.setLocationRelativeTo(null);
         time = new Timer(5, this);
-        panel = new TwinPanel();
+        panel = new ChaosPanel();
         panel.setBackground(Color.BLACK);
         frame.add(panel);
-
         frame.setVisible(true);
-        time.start();
     }
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
-        panel.repaint();
+        if (actionEvent.getSource() == time) {
+            panel.repaint();
+        } else {
+            if (actionEvent.getSource() == normal){
+
+            } else if (actionEvent.getSource() == chaos) {
+
+            } else if (actionEvent.getSource() == twin) {
+
+            } else if (actionEvent.getSource() == gravity) {
+
+            }
+            time.start();
+        }
     }
 }
