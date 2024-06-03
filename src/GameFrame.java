62e0396074790a6c8ec20ad4d4e2f013520b2950
BT_1N3_09 <sample@bths.edu>
2024-06-03 09:26:14 -0400

    ethan was here

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -13,14 +13,25 @@ public class GameFrame implements ActionListener {
     private JButton gravity;
     private JFrame frame;
     public GameFrame() {
+        panel = new JPanel();
         frame = new JFrame("Pong");
+        normal = new JButton("Normal");
+        twin = new JButton("Twin");
+        chaos = new JButton("Chaos");
+        gravity = new JButton("Gravity");
+        normal.addActionListener(this);
+        twin.addActionListener(this);
+        chaos.addActionListener(this);
+        gravity.addActionListener(this);
+        panel.add(twin);
+        panel.add(chaos);
+        panel.add(gravity);
+        panel.add(normal);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1500, 1000);
+        frame.setSize(300, 110);
         frame.setLocationRelativeTo(null);
-        time = new Timer(5, this);
-        panel = new ChaosPanel();
-        panel.setBackground(Color.BLACK);
         frame.add(panel);
+        time = new Timer(5, this);
         frame.setVisible(true);
     }
     @Override
@@ -28,15 +39,25 @@ public class GameFrame implements ActionListener {
         if (actionEvent.getSource() == time) {
             panel.repaint();
         } else {
-            if (actionEvent.getSource() == normal){
-
+            frame.setVisible(false);
+            frame = new JFrame();
+            if (actionEvent.getSource() == normal) {
+                panel = new NormalPanel();
             } else if (actionEvent.getSource() == chaos) {
-
+                panel = new ChaosPanel();
             } else if (actionEvent.getSource() == twin) {
-
+                panel = new TwinPanel();
             } else if (actionEvent.getSource() == gravity) {
-
+                panel = new GravityPanel();
             }
+            frame = new JFrame("Pong");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(1500, 1000);
+            frame.setLocationRelativeTo(null);
+            panel.setBackground(Color.BLACK);
+            frame.add(panel);
+
+            frame.setVisible(true);
             time.start();
         }
     }
